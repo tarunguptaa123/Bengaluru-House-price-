5ddc1b5b53099b5d613d0ba5357a0346212730bc
aakashy-013 <87707579+aakashy-013@users.noreply.github.com>
2022-06-11 22:40:21 +0530

    Update setup.sh

--- webapp/setup.sh
+++ webapp/setup.sh
@@ -1,13 +1,6 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"aayushthecool2411@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+echo "[server]
+headless = true
+port = $PORT
+enableCORS=false
+" >> ~/.streamlit/config.toml
